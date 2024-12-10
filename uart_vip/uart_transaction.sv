9fe51a3a076d1c0f33ff1dfd7a94f98bef7bd32a
phuchungpt <phuchungdcdt@gmail.com>
2024-12-10 23:27:48 +0700

    Update uart_transaction.sv

--- uart_vip/uart_transaction.sv
+++ uart_vip/uart_transaction.sv
@@ -4,11 +4,11 @@ class uart_transaction extends uvm sequence_item;
   bit      [8:0] receive_data;
   bit            parity_bit;
 
-  `uvm object_utils_begin (uart_transaction)
-    `uvm field_int (send_data            , UVM_ALL_ON |UVM_BIN)
-    `uvm field_int (receive_data         , UVM_ALL_ON |UVM_BIN)
-    `uvm field_int (parity_bit           , UVM_ALL_ON |UVM_BIN)
-  `uvm object_utils_end
+  `uvm_object_utils_begin (uart_transaction)
+    `uvm_field_int (send_data            , UVM_ALL_ON |UVM_BIN)
+    `uvm_field_int (receive_data         , UVM_ALL_ON |UVM_BIN)
+    `uvm_field_int (parity_bit           , UVM_ALL_ON |UVM_BIN)
+  `uvm_object_utils_end
 
   function new(string name="uart_transaction");
     super.new(name);
