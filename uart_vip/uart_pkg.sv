bfb6147387b2a5bd5e2a3fc708af9298e999dfe5
phuchungpt <phuchungdcdt@gmail.com>
2024-12-10 23:19:02 +0700

    Update uart_pkg.sv

--- uart_vip/uart_pkg.sv
+++ uart_vip/uart_pkg.sv
@@ -1,1 +1,30 @@
+//====================================
+// Project     : UART VIP
+//====================================
+// Filename    : uart_pkg.sv 
+// Author      : Phuc Hung
+// Date        : 2-Dec-2024
+//====================================
+// Description :
+//
+//
+//====================================
 
+`ifndef GUARD_UART_PKG_SV
+`define GUARD_UART_PKG_SV
+
+package uart_pkg;
+   import uvm_pkg::*;
+  
+   `include "uart_error_catcher.sv" 
+   `include "uart_config.sv" 
+   `include "uart_define.sv"
+   `include "uart_transaction.sv"
+   `include "uart_sequencer.sv"
+   `include "uart_driver.sv"
+   `include "uart_monitor.sv"
+   `include "uart_agent.sv"
+
+endpackage: uart_pkg
+
+`endif
