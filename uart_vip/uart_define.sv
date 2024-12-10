ad84250f9b3824f2459746565a908e369fdb3352
phuchungpt <phuchungdcdt@gmail.com>
2024-12-10 22:55:18 +0700

    Update uart_define.sv

--- uart_vip/uart_define.sv
+++ uart_vip/uart_define.sv
@@ -1,1 +1,23 @@
+// ==================================================
+// project : uart vip
+// ==================================================
+// fiLename : uart_define.sv
+// author   : phuchung
+// emaiL    : phuchung12tn4@gmai.com
+// date     : 6-dec-2024
+// ==================================================
+// description : define can override by environment
+//
+// =================================================
 
+`ifndef quard uart define_ sv
+`define quard_uart define_ sv
+
+  `ifndef fork_quard_begin
+    `define fork_quard_begin fork begin
+  `endif
+    
+  `ifndef fork_quard_end
+    `define fork_quard_end fork end
+  `endif
+`endif
